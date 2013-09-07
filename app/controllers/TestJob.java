430c8a157ffa28596fb1ab9d38705279eca0a7f0
nattaporn <gogorenger@gmail.com>
2013-09-07 19:29:06 +0700

    add TestJob

---
+++ app/controllers/TestJob.java
@@ -0,0 +1,11 @@
+package controllers;
+
+public class TestJob extends Application {
+	public static void testJob(String jobName, String discountCode , String ucrNo ){
+		render();
+	}
+	
+	public static void testResult(String jobName , String discountCode , String ucrNo , String ban, String subscriberNo){
+		render();
+	}
+}
