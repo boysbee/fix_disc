1fba4498b8e6fd19c2a216ebd32f63a050a14641
nattaporn <gogorenger@gmail.com>
2013-09-09 02:29:52 +0700

    don't use

--- app/models/BilingAccount.java
+++
@@ -1,9 +0,0 @@
-package models;
-
-import play.db.jpa.GenericModel;
-
-public class BilingAccount  extends GenericModel{
-	public int ban;
-	public String accountType;
-	public String accountCate;
-}
