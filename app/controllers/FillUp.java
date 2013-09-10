8eafbd66d1507b82b4457b17ac3d60bd37a49f00
nattaporn <gogorenger@gmail.com>
2013-09-11 01:02:54 +0700

    revise for add feature text suggestion with jobName,discountCode,ucrNo,keyword.

--- app/controllers/FillUp.java
+++ app/controllers/FillUp.java
@@ -1,26 +1,53 @@
 package controllers;
 
-import play.*;
-import static play.Logger.*;
-import java.util.*;
+import static play.Logger.info;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import util.ObjectUtils;
+
+import models.CsmDiscount;
 
 public class FillUp extends Application {
 	
 
 	public static void fill(String type,String query){
+		info("@@ type : %s , query : %s",type,query);
 		List<?> list = null;
 		if("jobName".equals(type)) {
 			list = searchJobName(query);
 		}
-
-		return renderJSON(list);
+		if("ucrNo".equals(type)){
+			list = searchUcrNo(query);
+		}
+		if("keyword".equals(type)) {
+			list = searchKeyword(query);
+		}
+		if("discountCode".equals(type)) {
+			list = searchDiscountCode(query);
+		}
+		renderJSON(list);
 	}
 	
-	private static List searchJobName(String query) throws Exception {
-		info("FillUp.searchJobName()");
-		
-		List<CsmDiscount> list = CsmDiscount.find("jobName like ?1" , "'%" + query + "%'").fetch();
-		return list;
+	private static List<?> searchDiscountCode(String query) {
+		List<CsmDiscount> list = CsmDiscount.find("lower(pk.discountCode) like ?1" , "%" + query.toLowerCase() + "%").fetch();
+		return list == null ? new ArrayList<CsmDiscount>() : list;
+	}
+
+	private static List<?> searchKeyword(String query) {
+		List<CsmDiscount> list = CsmDiscount.find("lower(keyword) like ?1" , "%" + query.toLowerCase() + "%").fetch();
+		return list == null ? new ArrayList<CsmDiscount>() : list;
+	}
+
+	private static List<?> searchUcrNo(String query) {
+		List<CsmDiscount> list = CsmDiscount.find("lower(pk.ucrNo) like ?1" , "%" + query.toLowerCase() + "%").fetch();
+		return list == null ? new ArrayList<CsmDiscount>() : list;
+	}
+
+	private static List<?> searchJobName(String query)  {
+		List<CsmDiscount> list = CsmDiscount.find("lower(pk.jobName) like ?1" , "%" + query.toLowerCase() + "%").fetch();
+		return list == null ? new ArrayList<CsmDiscount>() : list;
 	}
 	
 }
--- app/views/tags/addscript.tag
+++ app/views/tags/addscript.tag
@@ -1,3 +1,4 @@
-<script src="@{'/public/javascripts/jquery-1.6.4.min.js'}" type="text/javascript" charset="${_response_encoding}"></script>
+<script src="@{'/public/javascripts/jquery-1.8.2.min.js'}" type="text/javascript" charset="${_response_encoding}"></script>
 <script src="@{'/public/javascripts/bootstrap/js/bootstrap.min.js'}" type="text/javascript" charset="${_response_encoding}"></script>
+<script src="@{'/public/javascripts/bootstrap/js/bootstrap-typeahead.js'}" type="text/javascript" charset="${_response_encoding}"></script>
 <script src="@{'/public/javascripts/sessvars.js'}" type="text/javascript" charset="${_response_encoding}"></script>
