bd0f3bcfc89a1474846f41ae07cfde07b4764860
unknown <Natta86@VENDER1-CP2-F15.true.th>
2013-09-11 17:47:27 +0700

    add edit.html at views folder.

--- app/controllers/CsmDiscounts.java
+++ app/controllers/CsmDiscounts.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.List;
 
 import models.CsmDiscount;
+import models.CsmDiscountPK;
 import play.Logger;
 import util.DateUtil;
 
@@ -12,53 +13,60 @@ public class CsmDiscounts extends Application {
 		render();
 	}
 
-	public static void list(String jobName, String discountCode, String ucrNo,String keyword,
-			Integer size, Integer page) {
+	public static void list(String jobName, String discountCode, String ucrNo,
+			String keyword, Integer size, Integer page) {
 		Logger.info(
 				"@@ list -> job_name : %s , discount_code : %s , ucr_no : %s , keyword : %s",
-				jobName, discountCode, ucrNo,keyword);
+				jobName, discountCode, ucrNo, keyword);
 		page = page != null ? page : 1;
-		List<CsmDiscount> csmdiscounts =CsmDiscount.findList(jobName, discountCode, ucrNo, keyword, size, page);
+		List<CsmDiscount> csmdiscounts = CsmDiscount.findList(jobName,
+				discountCode, ucrNo, keyword, size, page);
 		render(csmdiscounts, jobName, discountCode, ucrNo, size, page);
 	}
 
 	public static void show(String jobName, String ucrNo, String discountCode) {
-		CsmDiscount item = CsmDiscount.find(
-				"pk.jobName = ?1 and pk.ucrNo = ?2 and pk.discountCode = ?3",
-				jobName, ucrNo, discountCode).first();
+		CsmDiscount item = CsmDiscount.findById(new CsmDiscountPK(jobName,
+				discountCode, ucrNo));
 		render(item);
 	}
 
-	
 	public static void add() {
 		CsmDiscount item = new CsmDiscount();
-	
 		render(item);
 	}
 
+	public static void edit(String jobName, String ucrNo, String discountCode) {
+		CsmDiscount item = CsmDiscount.findById(new CsmDiscountPK(jobName,
+				discountCode, ucrNo));
+		render(item);
 
-	
+	}
+
 	public static void save() {
 		Logger.info("##### start save ######");
 		CsmDiscount item = new CsmDiscount();
-		item.pk.jobName = request.params.get("item.pk.jobName");
-		item.pk.discountCode = request.params.get("item.pk.discountCode");
-		item.pk.ucrNo = request.params.get("item.pk.ucrNo");
-		item.devName = request.params.get("item.devName");
-		item.businessOwner = request.params.get("item.businessOwner");
-		item.soc = request.params.get("item.soc");
-		item.pp = request.params.get("item.pp");
-		item.propo = request.params.get("item.propo");
-		item.keyword = request.params.get("item.keyword");
-		item.accType = request.params.get("item.accType");
-		item.accCate = request.params.get("item.accCate");
-		item.actvCode = request.params.get("item.actvCode");
-		item.actvRsnCode = request.params.get("item.actvRsnCode");
-		item.benefit = request.params.get("item.benefit");
-		item.advancePayment = request.params.get("item.advancePayment");
-		String projectStartDate = request.params.get("item.projectStartDate");
-		String projectEndDate = request.params.get("item.projectStartDate");
-		item.remark = request.params.get("item.remark");
+		item.pk.jobName = params.get("item.pk.jobName");
+		item.pk.discountCode = params.get("item.pk.discountCode");
+		item.pk.ucrNo = params.get("item.pk.ucrNo");
+		item.devName = params.get("item.devName");
+		item.businessOwner = params.get("item.businessOwner");
+		item.soc = params.get("item.soc");
+		item.pp = params.get("item.pp");
+		item.propo = params.get("item.propo");
+		item.keyword = params.get("item.keyword");
+		item.accType = params.get("item.accType");
+		item.accCate = params.get("item.accCate");
+		if (item.accCate.lastIndexOf(",") > -1) {
+			item.accCate = item.accCate.substring(0,
+					(item.accCate.length() - 1));
+		}
+		item.actvCode = params.get("item.actvCode");
+		item.actvRsnCode = params.get("item.actvRsnCode");
+		item.benefit = params.get("item.benefit");
+		item.advancePayment = params.get("item.advancePayment");
+		String projectStartDate = params.get("item.projectStartDate");
+		String projectEndDate = params.get("item.projectStartDate");
+		item.remark = params.get("item.remark");
 		item.setProjectStartDate(DateUtil.convertDate(projectStartDate,
 				"yyyy-MM-dd", "dd/MM/yyyy"));
 		item.setProjectEndDate(DateUtil.convertDate(projectEndDate,
